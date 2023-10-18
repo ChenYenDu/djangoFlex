2ad029cdc207187c28fcd01005acf5089c1abef4
ChenYenDu <chun.yen.tu@gmail.com>
2023-10-19 00:56:33 +0800

    Fix test case typo

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Video
+
+class VideoModelTestCase(TestCase):
+    def setUp(self) -> None:
+        Video.objects.create(title='Test Title')
+        return super().setUp()
+    
+    def test_valid_title(self):
+        title = 'Test Title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+    
+    def test_created_count(self):
+        qs = Video.objects.all()
+        self.assertEqual(qs.count(), 1)
