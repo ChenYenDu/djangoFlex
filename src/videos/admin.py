ad3920cd0d90587cf3181e86dad7b42a1bcc7e35
ChenYenDu <43244178+ChenYenDu@users.noreply.github.com>
2023-11-02 00:12:31 +0800

    Task/02 video app initization (#2)
    * Add Video model:
    - title: char, max length 220
    - description: text
    - slug: slug
    - video_id: char, max length 220
    
    * Add VideoProxy
    
    * Fix test case typo
    
    * Register Video and VideoProxy to admin page
    
    * 02_video_app_initization
    - Fix video_id typo in Video model
    - Replace VideoProxy with VideoAllProxy and VideoPublishedProxy
    - Register VideoAllProxy and VideoPublishedProxy to admin page
    
    * 02_video_app_initization
    - Customize admin pager VideoAllProxyAdmin
    - Add is_published property to Video model

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,3 +1,41 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 
 # Register your models here.
+from .models import VideoAllProxy, VideoPublishedProxy
+
+class VideoAllProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', 'video_id', 'is_published']
+    search_fields = ['title']
+    list_filter = ['active']
+    readonly_fields = ['id', 'is_published']
+    class Meta:
+        model = VideoAllProxy
+    
+    # As general as publised is, it should goes to model instead in a single admin register 
+    # def published(self, obj, *args, **kwargs):
+    #     """
+    #     Input when cutomer views All Video page: (<VideoAllProxy: VideoAllProxy object (2)>,) {}
+    #     """
+    #     return obj.active
+
+    
+# register VideoAllProxy to admin page
+admin.site.register(VideoAllProxy, VideoAllProxyAdmin)
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+
+    class Meta:
+        model = VideoPublishedProxy
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return VideoPublishedProxy.objects.filter(active=True)
+        
+
+# register VideoPublishedProxy to admin page
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
