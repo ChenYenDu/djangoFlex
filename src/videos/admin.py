62eead8ca6024db86d36489af0d7f352e35b2a57
ChenYenDu <chun.yen.tu@gmail.com>
2023-11-02 00:54:23 +0800

    Add publish_timestamp and customize save functin for Video model

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -10,7 +10,7 @@ class VideoAllProxyAdmin(admin.ModelAdmin):
     list_display = ['title', 'id', 'video_id', 'state', 'is_published']
     search_fields = ['title']
     list_filter = ['state', 'active']
-    readonly_fields = ['id', 'is_published']
+    readonly_fields = ['id', 'is_published', 'publish_timestamp']
     class Meta:
         model = VideoAllProxy
     
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Video(models.Model):
@@ -14,10 +15,21 @@ class Video(models.Model):
     video_id = models.CharField(max_length=220)
     active = models.BooleanField(default=True)
     state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
     @property
     def is_published(self):
         return self.active
+
+    def save(self, *args, **kwargs) -> None:
+        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
+            print('save as timestamp for published')
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+
+        return super().save(*args, **kwargs)
+    
 
 class VideoAllProxy(Video):
     class Meta:
