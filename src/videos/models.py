5ac0c0a32234eeb32a3a1c27bf1205483acf2d04
ChenYenDu <chun.yen.tu@gmail.com>
2023-11-07 23:53:34 +0800

    Add test case for DRAFT and PUBLISH state

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,41 +1,49 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Video(models.Model):
     class VideoStateOptions(models.TextChoices):
         # This class defines the choices could be used in state field
         # It shows like this: CONSTANT = 'DB_VALUE', 'USER_DISPLAY_VALUE'
-        PUBLISH = 'PU', 'Publish'
-        DRAFT = 'DR', 'Draft'
+        PUBLISH = "PU", "Publish"
+        DRAFT = "DR", "Draft"
 
     title = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=220)
     active = models.BooleanField(default=True)
-    state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
-    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    state = models.CharField(
+        max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT
+    )
+    publish_timestamp = models.DateTimeField(
+        auto_now_add=False, auto_now=False, blank=True, null=True
+    )
 
     @property
     def is_published(self):
         return self.active
 
     def save(self, *args, **kwargs) -> None:
-        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
-            print('save as timestamp for published')
+        if (
+            self.state == self.VideoStateOptions.PUBLISH
+            and self.publish_timestamp is None
+        ):
             self.publish_timestamp = timezone.now()
         elif self.state == self.VideoStateOptions.DRAFT:
             self.publish_timestamp = None
 
         return super().save(*args, **kwargs)
-    
+
 
 class VideoAllProxy(Video):
     class Meta:
         proxy = True
         verbose_name = "All Video"
         verbose_name_plural = "All Videos"
+
 
 class VideoPublishedProxy(Video):
     class Meta:
--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,17 +1,32 @@
 from django.test import TestCase
-
+from django.utils import timezone
 from .models import Video
+
 
 class VideoModelTestCase(TestCase):
     def setUp(self) -> None:
-        Video.objects.create(title='Test Title')
+        Video.objects.create(title="Test Title")
+        Video.objects.create(
+            title="Test Title 2", state=Video.VideoStateOptions.PUBLISH
+        )
         return super().setUp()
-    
+
     def test_valid_title(self):
-        title = 'Test Title'
+        title = "Test Title"
         qs = Video.objects.filter(title=title)
         self.assertTrue(qs.exists())
-    
+
     def test_created_count(self):
         qs = Video.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_draft_case(self):
+        qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
         self.assertEqual(qs.count(), 1)
+
+    def test_publish_case(self):
+        now = timezone.now()
+        published_qs = Video.objects.filter(
+            state=Video.VideoStateOptions.PUBLISH, publish_timestamp__lte=now
+        )
+        self.assertTrue(published_qs.exists())
