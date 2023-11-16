b56d6b9e513922ae1b972451a9379c6842105f3b
ChenYenDu <chun.yen.tu@gmail.com>
2023-11-16 23:38:12 +0800

    Move duplicated publish state pre-save function to signal

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.db.models.signals import pre_save
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.utils.text import slugify
 
 
 # Create your models here.
+class PublishStateOptions(models.TextChoices):
+    # This class defines the choices could be used in state field
+    # It shows like this: CONSTANT = 'DB_VALUE', 'USER_DISPLAY_VALUE'
+    PUBLISH = "PU", "Publish"
+    DRAFT = "DR", "Draft"
+
+
 class VideoQuerySet(models.QuerySet):
     def published(self):
         return self.filter(
-            state=Video.VideoStateOptions.PUBLISH, publish_timestamp__lte=timezone.now()
+            state=PublishStateOptions.PUBLISH, publish_timestamp__lte=timezone.now()
         )
 
 
@@ -21,19 +29,15 @@ class VideoManager(models.Manager):
 
 
 class Video(models.Model):
-    class VideoStateOptions(models.TextChoices):
-        # This class defines the choices could be used in state field
-        # It shows like this: CONSTANT = 'DB_VALUE', 'USER_DISPLAY_VALUE'
-        PUBLISH = "PU", "Publish"
-        DRAFT = "DR", "Draft"
-
     title = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=220, unique=True)
     active = models.BooleanField(default=True)
     state = models.CharField(
-        max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT
+        max_length=2,
+        choices=PublishStateOptions.choices,
+        default=PublishStateOptions.DRAFT,
     )
     timestamp = models.DateTimeField(
         auto_now_add=True
@@ -52,14 +56,6 @@ class Video(models.Model):
         return self.active
 
     def save(self, *args, **kwargs) -> None:
-        if (
-            self.state == self.VideoStateOptions.PUBLISH
-            and self.publish_timestamp is None
-        ):
-            self.publish_timestamp = timezone.now()
-        elif self.state == self.VideoStateOptions.DRAFT:
-            self.publish_timestamp = None
-
         if self.slug is None:
             self.slug = slugify(self.title)
 
@@ -78,3 +74,19 @@ class VideoPublishedProxy(Video):
         proxy = True
         verbose_name = "Published Video"
         verbose_name_plural = "Published Videos"
+
+
+# publish state pre_save signal receiver function
+def publish_state_pre_save(sender, instance, *args, **kwargs):
+    is_publish = instance.state == PublishStateOptions.PUBLISH
+    is_draft = instance.state == PublishStateOptions.DRAFT
+
+    if is_publish and instance.publish_timestamp is None:
+        # add publish_timestamp if is_publish is True and publish_timestamp field is empty
+        instance.publish_timestamp = timezone.now()
+    elif is_draft:
+        # remove publish_timestamp field when video state is DRAFT
+        instance.publish_timestamp = None
+
+
+pre_save.connect(publish_state_pre_save, sender=Video)
