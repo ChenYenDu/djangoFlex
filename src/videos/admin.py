80dd2fa06ca14d2dc39242573b976a13499db2ce
ChenYenDu <43244178+ChenYenDu@users.noreply.github.com>
2024-01-29 23:25:26 +0800

    Task/07 init playlist app (#8)
    * Setup playlists app with django cli
    
    * Copy videos app models to playlists models and make migrations
    
    * Add test case for basic playlist model
    
    * Add foreign key 'video' to Playlist model and update test case
    
    * task/07_playlist_and_foreign_key

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -6,36 +6,45 @@ from django.http.request import HttpRequest
 # Register your models here.
 from .models import VideoAllProxy, VideoPublishedProxy
 
+
 class VideoAllProxyAdmin(admin.ModelAdmin):
-    list_display = ['title', 'id', 'video_id', 'state', 'is_published']
-    search_fields = ['title']
-    list_filter = ['state', 'active']
-    readonly_fields = ['id', 'is_published', 'publish_timestamp']
+    list_display = [
+        "title",
+        "id",
+        "video_id",
+        "state",
+        "is_published",
+        "get_playlist_ids",
+    ]
+    search_fields = ["title"]
+    list_filter = ["state", "active"]
+    readonly_fields = ["id", "is_published", "publish_timestamp", "get_playlist_ids"]
+
     class Meta:
         model = VideoAllProxy
-    
-    # As general as publised is, it should goes to model instead in a single admin register 
+
+    # As general as publised is, it should goes to model instead in a single admin register
     # def published(self, obj, *args, **kwargs):
     #     """
     #     Input when cutomer views All Video page: (<VideoAllProxy: VideoAllProxy object (2)>,) {}
     #     """
     #     return obj.active
 
-    
+
 # register VideoAllProxy to admin page
 admin.site.register(VideoAllProxy, VideoAllProxyAdmin)
 
 
 class VideoPublishedProxyAdmin(admin.ModelAdmin):
-    list_display = ['title', 'video_id']
-    search_fields = ['title']
+    list_display = ["title", "video_id"]
+    search_fields = ["title"]
 
     class Meta:
         model = VideoPublishedProxy
-    
+
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return VideoPublishedProxy.objects.filter(active=True)
-        
+
 
 # register VideoPublishedProxy to admin page
 admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
